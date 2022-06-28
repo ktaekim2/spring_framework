6dc4e5e3583128eaeb83960f9418636fe67abf3c
ktaekim2 <ktaekim2@gmail.com>
2022-06-28 16:59:46 +0900

    ???머지

---
+++ Board_20220523/src/test/java/com/its/board/Board_20220624/Board20220624ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.its.board.Board_20220624;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Board20220624ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
